70c019e33a456368bd4a8eeba3d0149c858b561c
tzt3315 <240335312@qq.com>
2018-01-23 14:17:38 +0800

    first commit

---
+++ SpecPods/ZTLog.h
@@ -0,0 +1,15 @@
+//
+//  ZTLog.h
+//  SpecPods
+//
+//  Created by tab on 2018/1/23.
+//  Copyright © 2018年 Alec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZTLog : NSObject
+
++ (void)hello;
+
+@end
---
+++ SpecPods/ZTLog.m
@@ -0,0 +1,17 @@
+//
+//  ZTLog.m
+//  SpecPods
+//
+//  Created by tab on 2018/1/23.
+//  Copyright © 2018年 Alec. All rights reserved.
+//
+
+#import "ZTLog.h"
+
+@implementation ZTLog
+
++ (void)hello{
+    NSLog(@"hello world");
+}
+
+@end
